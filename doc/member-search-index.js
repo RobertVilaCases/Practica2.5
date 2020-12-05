df85f10ecb64e29f945675c12474e82ea32e2007
RobertVilaCases <robert.vc.dam2@alumnescostafreda.cat>
2020-12-05 18:29:58 +0100

    versio2

---
+++ doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"Exercici5_RobertVila","c":"Exercici5_RobertVila","l":"Exercici5_RobertVila()","url":"%3Cinit%3E()"},{"p":"Exercici5_RobertVila","c":"Exercici5_RobertVila","l":"main(String[])","url":"main(java.lang.String[])"}]
---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"Exercici5_RobertVila","l":"Exercici5_RobertVila"}]
